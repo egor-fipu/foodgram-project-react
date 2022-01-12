24ff260727b8b9b9d49cd5899c8891b473cfb2a7
Егор Коростелев <egor.fipu@gmail.com>
2022-01-12 22:27:45 +0700

    add backend files

---
+++ backend/recipes/services.py
@@ -0,0 +1,24 @@
+from django.db.models import Sum
+from django.http import HttpResponse
+
+from .models import IngredientForRecipe
+
+
+def get_send_file(request, filename):
+    user = request.user
+    ingredients = IngredientForRecipe.objects.filter(
+        recipe__purchase__user=user
+    ).values(
+        'ingredient__name', 'ingredient__measurement_unit'
+    ).annotate(
+        ingreient_total=Sum('amount')
+    )
+    text = 'СПИСОК ПОКУПОК:\n'
+    for number, ingredient in enumerate(ingredients, start=1):
+        text += (f'{number}) '
+                 f'{ingredient["ingredient__name"]}: '
+                 f'{ingredient["ingreient_total"]} '
+                 f'{ingredient["ingredient__measurement_unit"]}\n')
+    response = HttpResponse(text, content_type='text/plain')
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+    return response
---
+++ backend/users/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from djoser import views
+
+from users.views import FollowApiView, ListFollowViewSet
+
+urlpatterns = [
+    path(
+        'auth/token/login/',
+        views.TokenCreateView.as_view(), name='login'
+    ),
+    path(
+        'auth/token/logout/',
+        views.TokenDestroyView.as_view(), name='logout'
+    ),
+    path(
+        'users/<int:pk>/subscribe/',
+        FollowApiView.as_view(), name='subscribe'
+    ),
+    path(
+        'users/subscriptions/',
+        ListFollowViewSet.as_view(), name='subscription'
+    ),
+    path(
+        '', include('djoser.urls')
+    ),
+]
